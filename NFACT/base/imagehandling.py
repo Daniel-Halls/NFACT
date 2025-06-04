e16e66c1a8593ecd4dc16153372ad1bcbbf3e992
Daniel-Halls <mszdjh3@nottingham.ac.uk>
2025-06-04 14:06:56 +0100

    added three functions to get data from cifti data

--- NFACT/base/imagehandling.py
+++ NFACT/base/imagehandling.py
@@ -573,3 +573,82 @@ def save_cifti(
         brainmodel = add_nifti(seeds[2:], brainmodel, coords)
     dscalar_object = create_dscalar(grey_comp, brainmodel)
     nb.save(dscalar_object, save_path)
+
+
+def surf_data_from_cifti(data: np.ndarray, axis: object, surf_name: str) -> np.ndarray:
+    """
+    Function to get surface data from cifti
+
+    Parameters
+    ----------
+    data: np.ndarray
+        np.array of imaging data
+    axis: nbabel.cifti2.cifti2_axes.BrainModelAxis
+    surf_name: str
+        string of surface name.
+
+    Returns
+    -------
+    surf_data: np.ndarray
+        np.array of surface data
+    """
+    for name, data_indices, model in axis.iter_structures():
+        if name == surf_name:
+            data = data.T[data_indices]
+            vtx_indices = model.vertex
+            surf_data = np.zeros(
+                (vtx_indices.max() + 1,) + data.shape[1:], dtype=data.dtype
+            )
+            surf_data[vtx_indices] = data
+            return surf_data
+
+
+def volume_from_cifti(data: np.ndarray, axis: object) -> object:
+    """
+    Function to return the nifti from a cifti
+
+    Parameters
+    ----------
+    data: np.ndarray
+        np.array of imaging data
+    axis: nbabel.cifti2.cifti2_axes.BrainModelAxis
+
+    Returns
+    -------
+    object: nb.Nifti1Image
+        nifti image
+    """
+    assert isinstance(axis, nb.cifti2.BrainModelAxis)
+    data = data.T[axis.volume_mask]
+    vox_indices = tuple(axis.voxel[axis.volume_mask].T)
+    vol_data = np.zeros(axis.volume_shape + data.shape[1:], dtype=data.dtype)
+    vol_data[vox_indices] = data
+    return nb.Nifti1Image(vol_data, axis.affine)
+
+
+def decompose_cifti(img: object) -> dict:
+    """
+    Function to decompose a loaded cifti
+
+    Parameters
+    ----------
+    img: object
+        loaded cifit object
+
+    Returns
+    -------
+    dict: dictionary
+        dict of volume and L/R
+        surfaces
+    """
+    data = img.get_fdata(dtype=np.float32)
+    brain_models = img.header.get_axis(1)  # Assume we know this
+    return {
+        "vol": volume_from_cifti(data, brain_models),
+        "L_surf": surf_data_from_cifti(
+            data, brain_models, "CIFTI_STRUCTURE_CORTEX_LEFT"
+        ),
+        "R_surf": surf_data_from_cifti(
+            data, brain_models, "CIFTI_STRUCTURE_CORTEX_RIGHT"
+        ),
+    }
